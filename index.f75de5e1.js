9bfd6c5f54dd4ef76cb1022ad99535d1d7cf1d9e
dima91020 <vereskodima76@gmail.com>
2025-06-30 12:17:55 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,34 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    // WRITE YOUR CODE HERE
+    if (Object.keys(data).length > 0) {
+        const ul = document.createElement("ul");
+        for (const [key, value] of Object.entries(data)){
+            const li = document.createElement("li");
+            li.textContent = key;
+            if (Object.keys(value).length > 0) createTree(li, value);
+            ul.append(li);
+        }
+        element.append(ul);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
